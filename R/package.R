6aba48db12cc111e688b1aaa5bec8e93fdb2abf4
rCarto <timothee.giraud@cnrs.fr>
2026-06-08 10:43:28 +0200

    add pkg level documentation

---
+++ R/package.R
@@ -0,0 +1,16 @@
+#' @title Package description
+#' @name geoviz
+#' @description
+#' Create a wide range of interactive, zoomable vector maps.
+#' This package is an R binding for the geoviz javascript library
+#' (https://github.com/riatelab/geoviz), itself based on the
+#' d3.js ecosystem ported by Mike Bostock (https://d3js.org/).
+#' Like the original javascript library, the package takes advantage
+#' of d3's many features: proportional symbols, pictograms, typologies,
+#' choropleth maps, Spikes, tiles, Dorling cartograms, etc. It can also be used
+#' to create pretty static vectorial maps in svg format, suitable for editorial
+#' cartography.
+#
+#'
+#' @docType package
+"_PACKAGE"
